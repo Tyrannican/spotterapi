556460b6aac0b705e3ae2c1c318aac2b778756dc
Graham Keenan <graham.keenan@outlook.com>
2023-07-06 18:03:58 +0100

    added migrations and deps for postgres

--- migrations/20230706145854_create_sighting_table.sql
+++ migrations/20230706145854_create_sighting_table.sql
@@ -1,1 +1,11 @@
 -- Add migration script here
+CREATE TABLE sightings(
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    user_id TEXT NOT NULL,
+    lat FLOAT NOT NULL,
+    lng FLOAT NOT NULL,
+    object TEXT NOT NULL,
+    description TEXT,
+    created_at timestamp NOT NULL
+);
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -20,13 +20,16 @@ DB_PASSWORD=${POSTGRES_PASSWORD:=password}
 DB_NAME=${POSTGRES_DB:=spotter}
 DB_PORT=${POSTGRES_PORT:=5432}
 
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+fi
 
 # Make sure that Postgres is available before creating it
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c "\q"; do
@@ -41,4 +44,6 @@ DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME
 export DATABASE_URL
 
 sqlx database create
-sqlx migrate add create_sighting_table
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated and ready to go!"
