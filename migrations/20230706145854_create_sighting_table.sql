0870a4946dbccd283e93ef86fc8a0c980a7a0f75
Graham Keenan <graham.keenan01@bbc.co.uk>
2023-07-17 18:30:50 +0100

    modified migration to create new DB layout

--- migrations/20230706145854_create_sighting_table.sql
+++ migrations/20230706145854_create_sighting_table.sql
@@ -1,11 +1,30 @@
--- Add migration script here
-CREATE TABLE sightings(
-    id TEXT NOT NULL,
-    PRIMARY KEY (id),
-    user_id TEXT NOT NULL,
+CREATE TABLE thing_type(
+    id INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    type VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE thing(
+    id UUID NOT NULL,
+    PRIMARY KEY(id),
+    user_id UUID,
+    type INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
     lat FLOAT NOT NULL,
     lng FLOAT NOT NULL,
-    object TEXT NOT NULL,
+    count INTEGER NOT NULL,
     description TEXT,
-    created_at bigint NOT NULL
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(type) REFERENCES thing_type(id)
 );
+
+
+INSERT INTO thing_type (id, type) VALUES
+    (0, 'Object'),
+    (1, 'Mammal'),
+    (2, 'Plant'),
+    (3, 'Fungi'),
+    (4, 'Insect'),
+    (5, 'Bird'),
+    (6, 'Reptile'),
+    (7, 'Marine')
